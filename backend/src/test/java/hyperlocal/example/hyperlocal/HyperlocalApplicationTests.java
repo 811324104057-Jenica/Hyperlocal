96db667cf16cdf4c9af6dfe1dd01a8d3313db72a
811324104057-Jenica <811324104057@jjcet.ac.in>
2026-08-23 12:15:22 +0530

    Added new file

---
+++ backend/src/test/java/hyperlocal/example/hyperlocal/HyperlocalApplicationTests.java
@@ -0,0 +1,13 @@
+package hyperlocal.example.hyperlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperlocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
