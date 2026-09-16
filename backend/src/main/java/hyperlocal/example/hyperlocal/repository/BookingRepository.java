4efad2716c863407c771bd6325d3df10a0a759d5
811324104057-Jenica <811324104057@jjcet.ac.in>
2026-09-16 21:24:05 +0530

    Add booking repository

---
+++ backend/src/main/java/hyperlocal/example/hyperlocal/repository/BookingRepository.java
@@ -0,0 +1,12 @@
+package hyperlocal.example.hyperlocal.repository;
+
+import hyperlocal.example.hyperlocal.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByCustomerId(Long customerId);
+
+}
