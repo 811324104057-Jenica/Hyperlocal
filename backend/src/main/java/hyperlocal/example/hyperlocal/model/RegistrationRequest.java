58f290ad9c49e19371d45af52967001dccaf042f
811324104057-Jenica <811324104057@jjcet.ac.in>
2026-09-21 06:19:34 +0530

    Add customer registration request model

---
+++ backend/src/main/java/hyperlocal/example/hyperlocal/model/RegistrationRequest.java
@@ -0,0 +1,91 @@
+package hyperlocal.example.hyperlocal.model;
+
+public class RegistrationRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    private String service;
+    private double price;
+    private int experience;
+
+    private double latitude;
+    private double longitude;
+
+    public RegistrationRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
